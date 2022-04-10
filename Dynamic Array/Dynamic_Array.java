145c11b1d6a992700560e6b0a9e1dce42839c654
Mrityunjay029 <77934726+Mrityunjay029@users.noreply.github.com>
2022-04-10 15:17:24 +0530

    Create Dynamic_Array.java

---
+++ Dynamic Array/Dynamic_Array.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Dynamic_Array {
+    public static void main(String[] args){
+        Dynamic_Array_Class numbers = new Dynamic_Array_Class(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.display();
+    }
+}
